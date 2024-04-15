5b9039404d10dd2558e599266f2106d38d98fa82
DiegoAlessandroMotta <diegomottaq@gmail.com>
2024-04-15 17:42:07 -0500

    Add Employee sort constants

---
+++ src/employees/EmployeeConstants.ts
@@ -0,0 +1,13 @@
+export const EmployeeSortStringConst = {
+  NAME: 'name',
+  LAST_NAME: 'lastName',
+  EMAIL: 'email'
+} as const
+
+export const EmployeeSortNumberConst = {
+  SALARY: 'salary'
+} as const
+
+export const EmployeeSortDateConst = {
+  HIRING_DATE: 'hiringDate'
+} as const
