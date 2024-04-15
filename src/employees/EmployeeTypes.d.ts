571ad38b01a9b4c9ce42b3b407306c3152a41705
DiegoAlessandroMotta <diegomottaq@gmail.com>
2024-04-15 17:40:56 -0500

    Add Employee sort types

--- src/employees/EmployeeTypes.d.ts
+++ src/employees/EmployeeTypes.d.ts
@@ -1,16 +1,6 @@
 import { type z } from 'zod'
+import { type EmployeeSortDateConst, type EmployeeSortNumberConst, type EmployeeSortStringConst } from './EmployeeConstants'
 import { type EmployeeSchema, type GetEmployeeQuerySchema } from './schemas/employees'
-
-// export interface Employee {
-//   name: string
-//   lastName: string
-//   dni: string
-//   phone: string
-//   email: string
-//   hiringDate: string
-//   salary: number
-//   positionId: string
-// }
 
 export type Employee = z.infer<typeof EmployeeSchema>
 
@@ -19,12 +9,6 @@ export type EmployeeId = string /* ${string}-${string}-${string}-${string}-${str
 export interface EmployeeWithId extends Employee {
   id: EmployeeId
 }
-
-// interface GetEmployeesParams {
-//   limit?: number
-//   sort_by?: string
-//   // sort?: sortType
-// }
 
 export type GetEmployeesParams = z.infer<typeof GetEmployeeQuerySchema>
 
@@ -44,3 +28,11 @@ export interface UpdateParams {
   id: EmployeeId
   input: Partial<EmployeeWithId>
 }
+
+export type EmployeeSortString = typeof EmployeeSortStringConst[keyof typeof EmployeeSortStringConst]
+
+export type EmployeeSortNumber = typeof EmployeeSortNumberConst[keyof typeof EmployeeSortNumberConst]
+
+export type EmployeeSortDate = typeof EmployeeSortDateConst[keyof typeof EmployeeSortDateConst]
+
+export type EmployeeSort = EmployeeSortString | EmployeeSortNumber | EmployeeSortDate
