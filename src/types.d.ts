05f595b070db5a1c4c6364077e4f25a9120b1a18
Xanderitsme <xanderitsme@gmail.com>
2024-04-16 14:57:38 -0500

    Add models type

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,7 +1,24 @@
+import { type localModels } from './server/server-with-local'
+
 declare global {
   interface Array<T> {
     toSorted(compareFn?: ((a: T, b: T) => number) | undefined): T[]
   }
 }
 
-export type Comment = string
+export type Models = typeof localModels
+
+// export interface GenericModelType<
+//   T,
+//   GetParams,
+//   GetByIdParams,
+//   CreateParams,
+//   DeleteParams,
+//   UpdateParams
+// > {
+//   get: (params: GetParams) => Promise<T[]>
+//   getById: ({ id }: GetByIdParams) => Promise<T | undefined>
+//   create: ({ input }: CreateParams) => Promise<T>
+//   delete: ({ id }: DeleteParams) => Promise<boolean>
+//   update: ({ id, input }: UpdateParams) => Promise<false | T>
+// }
