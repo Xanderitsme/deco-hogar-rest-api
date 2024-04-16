6297c6b59fd2b0d1f494c10e47b206ee5388b4eb
Xanderitsme <xanderitsme@gmail.com>
2024-04-16 16:12:23 -0500

    Move files directory

---
+++ src/employees/Employees.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express'
+import { type EmployeeModelType } from './EmployeeTypes'
+import { EmployeeController } from './controller'
+
+export const createEmployeeRouter = ({ EmployeeModel }: { EmployeeModel: EmployeeModelType }) => {
+  const employeeRouter = Router()
+
+  const employeeController = new EmployeeController(EmployeeModel)
+
+  employeeRouter.get('/', employeeController.getEmployees)
+  employeeRouter.get('/:id', employeeController.getById)
+  employeeRouter.post('/', employeeController.create)
+  employeeRouter.delete('/:id', employeeController.delete)
+  employeeRouter.patch('/:id', employeeController.update)
+
+  return employeeRouter
+}
